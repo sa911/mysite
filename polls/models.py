357f851f0106f273a4dcd5a4f482ad7d63e36029
sa911@users.noreply.github.com <sa911>
2015-09-27 18:24:31 +0530

    finished tutorial 2. mostly glanced through admin sections. onto tutorial 3

--- polls/models.py
+++ polls/models.py
@@ -8,10 +8,14 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date published')
 
 	def __str__(self):
-		return "%s %s" % (self.question_text, self.pub_date)
+		return "%s" % (self.question_text)
 	
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
